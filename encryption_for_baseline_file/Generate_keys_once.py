40c0a8231f7fd869df6ae43ed50e7d44c82dbb20
AdityaPatadiya <patadiyaaditya0079@gmail.com>
2025-01-01 17:47:57 +0530

    delete unnecessary files for github.

--- encryption_for_baseline_file/Generate_keys_once.py
+++
@@ -1,8 +0,0 @@
-from baseline_security import generate_key
-from digital_signature import generate_key_pair
-
-# Generate the encryption key
-generate_key()
-
-# Generate RSA key pair for digital signatures
-generate_key_pair()
